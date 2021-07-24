321619e04729563c99d9c0951eec2b67b2a3e8f0
zjun <565061763@qq.com>
2021-07-24 16:34:17 +0800

    refactor

--- app/src/main/java/com/sd/demo/mvvm/MainActivity.kt
+++ app/src/main/java/com/sd/demo/mvvm/MainActivity.kt
@@ -2,27 +2,27 @@ package com.sd.demo.mvvm
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
-import com.sd.lib.mvvm.fLifecycle
+import com.sd.demo.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     val TAG = MainActivity::class.java.simpleName
+    private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
 
-        findViewById<View>(R.id.view_root).fLifecycle()
-            .addObserver(object : LifecycleEventObserver {
-                override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-                    Log.i(TAG, "onStateChanged event:${event}")
-                }
-            })
+        _binding.testLayout.lifecycle.addObserver(object : LifecycleEventObserver {
+            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                Log.i(TAG, "TestLayout onStateChanged event:${event}")
+            }
+        })
 
         ProcessLifecycleOwner.get().lifecycle.addObserver(object : LifecycleEventObserver {
             override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
